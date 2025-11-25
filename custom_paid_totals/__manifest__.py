5fd0f647789c1bd2aba64fdcdabbdebdea16f521
Jeranut Mickael <jeranutmickael@gmail.com>
2025-11-25 08:34:13 +0300

    journal mobile money complet

--- custom_paid_totals/__manifest__.py
+++ custom_paid_totals/__manifest__.py
@@ -8,6 +8,7 @@
     'data': [
         'security/ir.model.access.csv',
         'views/paid_totals_views.xml',
+        'views/paid_totals_views_mobile.xml',
         'views/journal_libel.xml',
         'views/invoice_date_read_only.xml',
     ],
