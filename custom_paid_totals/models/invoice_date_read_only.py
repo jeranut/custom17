730fb0e96590fee3831b14273d263e3ee75c83c6
Jeranut Mickael <jeranutmickael@gmail.com>
2025-11-27 06:06:41 +0300

    Update invoice_date_read_only logic

--- custom_paid_totals/models/invoice_date_read_only.py
+++ custom_paid_totals/models/invoice_date_read_only.py
@@ -16,3 +16,12 @@ class AccountMove(models.Model):
         res = super(AccountMove, self).default_get(fields_list)
         res['invoice_date'] = date.today()
         return res
+
+    def action_register_payment(self):
+        # Mettre à jour automatiquement invoice_date si différente de today's date
+        for move in self:
+            if move.invoice_date != date.today():
+                move.write({'invoice_date': date.today()})
+
+        # ensuite appeler le comportement normal
+        return super(AccountMove, self).action_register_payment()
